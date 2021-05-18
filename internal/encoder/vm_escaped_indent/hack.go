aee5d8a6fbd0abd26a871b88899216192a87695a
Masaaki Goshima <goccy54@gmail.com>
2021-05-18 14:48:13 +0900

    Refactor Debug mode for encoding

--- internal/encoder/vm_escaped_indent/hack.go
+++ internal/encoder/vm_escaped_indent/hack.go
@@ -1,9 +1,6 @@
 package vm_escaped_indent
 
-import (
-	// HACK: compile order
-	// `vm`, `vm_escaped`, `vm_indent`, `vm_escaped_indent`, `vm_debug` packages uses a lot of memory to compile,
-	// so forcibly make dependencies and avoid compiling in concurrent.
-	// dependency order: vm => vm_escaped => vm_indent => vm_escaped_indent => vm_debug
-	_ "github.com/goccy/go-json/internal/encoder/vm_debug"
-)
+// HACK: compile order
+// `vm`, `vm_escaped`, `vm_indent`, `vm_escaped_indent` packages uses a lot of memory to compile,
+// so forcibly make dependencies and avoid compiling in concurrent.
+// dependency order: vm => vm_escaped => vm_indent => vm_escaped_indent

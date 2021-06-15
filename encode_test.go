94cecc06094f8b5fb3f487563fd7b8131573655e
IncSW <dev@incsw.in>
2021-06-15 19:10:56 +0300

    update tests

--- encode_test.go
+++ encode_test.go
@@ -1958,19 +1958,3 @@ func TestEncodeContextOption(t *testing.T) {
 		}
 	})
 }
-
-func TestIssue251(t *testing.T) {
-	array := [3]int{1, 2, 3}
-	err := stdjson.Unmarshal([]byte("[ ]"), &array)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(array)
-
-	array = [3]int{1, 2, 3}
-	err = json.Unmarshal([]byte("[ ]"), &array)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(array)
-}

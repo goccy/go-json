8f39c9039e2fc2e197a2e505b45021e3b714c5bc
Masaaki Goshima <goccy54@gmail.com>
2023-10-13 12:27:31 +0900

    update golangci-lint

--- internal/encoder/string.go
+++ internal/encoder/string.go
@@ -1,7 +1,7 @@
 // This files's string processing codes are inspired by https://github.com/segmentio/encoding.
 // The license notation is as follows.
 //
-// MIT License
+// # MIT License
 //
 // Copyright (c) 2019 Segment.io, Inc.
 //
@@ -22,7 +22,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-//
 package encoder
 
 import (

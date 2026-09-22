7c1b55a3e2f2b5a4928d6521a62b0fbdccf1aaf5
Masaaki Goshima <goccy54@gmail.com>
2026-09-22 10:50:14 +0900

    Revert "Do what the head of a struct does in the opcode of an element of a slice"
    This reverts commit f1e43ebaa5b58a73ba25e2fdec00494bea3bcd1b.

--- internal/encoder/vm_color_indent/vm.go
+++ internal/encoder/vm_color_indent/vm.go
@@ -266,23 +266,8 @@ func Run(ctx *encoder.RuntimeContext, b []byte, codeSet *encoder.OpcodeSet) ([]b
 				storeInt(ctxptr, code.ElemIdx, idx)
 				data := load(ctxptr, code.Idx)
 				size := uintptr(code.Size)
-				p := unsafe.Add(data, idx*size)
-				isStruct := code.Flags&encoder.StructElemFlags != 0
 				code = code.Next
-				if isStruct {
-					// the element is a struct: what its head does is done here, and the head is skipped.
-					if code.Op == encoder.OpStructPtrHead {
-						p = ptrToNPtr(p, code.PtrNum)
-						if p == nil {
-							b = appendNullComma(ctx, b)
-							code = code.End.Next
-							break
-						}
-					}
-					b = appendStructHead(ctx, b)
-					code = code.Next
-				}
-				store(ctxptr, code.Idx, p)
+				store(ctxptr, code.Idx, unsafe.Add(data, idx*size))
 			} else {
 				b = appendArrayEnd(ctx, code, b)
 				code = code.End.Next
@@ -320,22 +305,8 @@ func Run(ctx *encoder.RuntimeContext, b []byte, codeSet *encoder.OpcodeSet) ([]b
 				storeInt(ctxptr, code.ElemIdx, idx)
 				p := load(ctxptr, code.Idx)
 				size := uintptr(code.Size)
-				p = unsafe.Add(p, idx*size)
-				isStruct := code.Flags&encoder.StructElemFlags != 0
 				code = code.Next
-				if isStruct {
-					if code.Op == encoder.OpStructPtrHead {
-						p = ptrToNPtr(p, code.PtrNum)
-						if p == nil {
-							b = appendNullComma(ctx, b)
-							code = code.End.Next
-							break
-						}
-					}
-					b = appendStructHead(ctx, b)
-					code = code.Next
-				}
-				store(ctxptr, code.Idx, p)
+				store(ctxptr, code.Idx, unsafe.Add(p, idx*size))
 			} else {
 				b = appendArrayEnd(ctx, code, b)
 				code = code.End.Next
